34342d9efd39aa96e30e6fc53c72ff04c357f91d
Link4Electronics <link4electronics@gmail.com>
2026-05-21 05:37:07 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/partitionmanager.svg
 export DESKTOP=/usr/share/applications/org.kde.partitionmanager.desktop
-export STARTUPWMCLASS=
+export STARTUPWMCLASS=org.kde.partitionmanager
 export DEPLOY_QT=1
 export QT_DIR=qt6
 
